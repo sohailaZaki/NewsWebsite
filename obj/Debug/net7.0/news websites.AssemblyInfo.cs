6915112696c92202799924a7221be15cc59e5ff3
sohaila <sohailazaki@gmail.com>
2024-07-10 20:42:58 +0300

    news of each category is added

--- obj/Debug/net7.0/news websites.AssemblyInfo.cs
+++ obj/Debug/net7.0/news websites.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("news websites")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea753b2beb0b0b17d70c342ef0d0b9404025bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0cc446fb4965417667b1890160491c0ad88317")]
 [assembly: System.Reflection.AssemblyProductAttribute("news websites")]
 [assembly: System.Reflection.AssemblyTitleAttribute("news websites")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
